1e115b9d3664b63d57261fa993d9d42b67ec3f24
arpg <araceli.prietogallego@yh.nackademin.se>
2023-01-10 19:58:03 +0100

    fixed access to book details page

--- src/BookList.js
+++ src/BookList.js
@@ -6,7 +6,7 @@ const BookList = ({ books }) => {
         <div className="book-list">
             {books.map((book) => (
                 <div className="book-card" key={book.id}>
-                    <Link to={`/books/${book.id}`}>
+                    <Link to={`https://cdn-icons-png.flaticon.com/512/3771/3771361.png/books/${book.id}`}>
                         <img src={book.cover} alt={book.title}/>
                     </Link>
                 </div>
